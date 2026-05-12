8f7def089b94359aff3dc4af3085343e764b10e6
turtlesvsratking-svg <turtles.vs.rat.king@gmail.com>
2026-05-12 20:10:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,11 @@ function addToCalendar() {
     const memo = document.getElementById('memo').value;
     const title = `【トレ】${currentSelection.name}`;
     
-    // 日付処理の修正：日本の現在時刻を取得
     const now = new Date();
     const y = now.getFullYear();
     const m = ("0" + (now.getMonth() + 1)).slice(-2);
     const d = ("0" + now.getDate()).slice(-2);
     
-    // Googleカレンダー形式 (YYYYMMDD)
     const dateStr = `${y}${m}${d}`;
     const dates = `${dateStr}/${dateStr}`;
 
